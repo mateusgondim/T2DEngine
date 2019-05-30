0df218d6e8571f9e21e1b606e8dc4e29067ef4f9
mateusgondim <mateusgondimlima@gmail.com>
2019-05-30 17:40:41 -0300

    Fix syntax error

--- engine/source/sid_preprocessor/source/src/main.hpp
+++ engine/source/sid_preprocessor/source/src/main.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <fstream>
 
-void preprocess_line(std::string  line, std::fstream & output_file_stream)
+void preprocess_line(std::string  line, std::fstream & output_file_stream);
 void sid_parser(const char * unprocessed_file_path, const char * preprocessed_file_path);
 
 #endif // !_SID_PREPROCESSOR_HPP
