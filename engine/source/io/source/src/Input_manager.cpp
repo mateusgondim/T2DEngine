03e95abb5ee3269979438753bd5373576e99399f
mateusgondim <mateusgondimlima@gmail.com>
2019-06-13 17:28:16 -0300

    Refactor Input_manager

--- engine/source/io/source/src/Input_manager.cpp
+++ engine/source/io/source/src/Input_manager.cpp
@@ -13,19 +13,19 @@ namespace io {
                 m_keyboard_state.update_buttons();
         }
 
-        bool Input_manager::get_button_down(unsigned action_index) const
+        bool Input_manager::get_button_down(uint32_t action_index) const
         {
                 uint64_t button_mask = m_keyboard_control_scheme.get_button_mask_from_action(action_index);
                 return m_keyboard_state.buttons_just_went_down(button_mask);
         }
 
-        bool Input_manager::get_button_up(unsigned action_index) const 
+        bool Input_manager::get_button_up(uint32_t action_index) const 
         {
                 uint64_t button_mask = m_keyboard_control_scheme.get_button_mask_from_action(action_index);
                 return m_keyboard_state.buttons_just_went_up(button_mask);
         }
 
-        bool Input_manager::get_button(unsigned action_index) const
+        bool Input_manager::get_button(uint32_t action_index) const
         {
                 uint64_t button_mask = m_keyboard_control_scheme.get_button_mask_from_action(action_index);
                 return m_keyboard_state.buttons_down(button_mask);
