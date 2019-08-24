b20d92cf46811d5f7dbc2dfc8c93d27c65b64fbb
mateusgondim <mateusgondimlima@gmail.com>
2019-08-24 12:49:08 -0300

    Refactor Player's constructor parameter list

--- game/source/src/Player.hpp
+++ game/source/src/Player.hpp
@@ -1,6 +1,7 @@
 #ifndef _PLAYER_HPP
 #define _PLAYER_HPP
 #include "Actor.hpp"
+#include <cstddef>
 
 namespace math { struct vec2; struct vec3; class mat4; }
 namespace physics_2d { struct AABB_2d; class Body_2d; class World; }
@@ -9,9 +10,9 @@ class Event;
 
 class Player final : public gom::Actor {
 public:
-        Player(const game_object_id unique_id, const uint16_t handle_index,
-               atlas_n_layer & sprite_data, physics_2d::Body_2d_def *pbody_def, 
-               const gfx::Animator_controller *pcontroller, bool facing_left = true);
+        Player(std::size_t object_sz, atlas_n_layer & sprite_data,
+               physics_2d::Body_2d_def *pbody_def, const gfx::Animator_controller *pcontroller,
+               bool facing_left = true);
 
         void            handle_input();
         void            update(const float dt) override;
