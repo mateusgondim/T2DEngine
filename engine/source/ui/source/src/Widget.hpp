4cd077416e6eccba374b48f6b2978cff9d023a9d
mateusgondim <mateusgondimlima@gmail.com>
2019-11-04 18:30:04 -0300

    Create the Widget class
    This commit, creates the Widget class, with the intial functionality

---
+++ engine/source/ui/source/src/Widget.hpp
@@ -0,0 +1,30 @@
+#ifndef _WIDGET_HPP
+
+#define _WIDGET_HPP
+
+#include "Game_object.hpp"
+#include "Rect.hpp"
+#include "Vertex1P1C1UV.hpp"
+
+class Event;
+namespace ui { class Canvas; }
+
+namespace ui
+{
+        class Widget : public gom::Game_object {
+                friend class Canvas;
+        public:
+                virtual void update(const float dt) override;
+                virtual void on_event(Event & event) override;
+        private:
+                explicit Widget(Canvas & parent_canvas);
+                 Widget(Canvas & parent_canvas, const math::Rect & rect);
+                // Image * pimage_componenet;
+                // Text  * ptext_component;
+                // Button * pbutton_component;
+                Canvas *         m_pparent_canvas;
+                math::Rect       m_rect;
+                void             get_view_space_vertices(gfx::Vertex1P1C1UV * pbuffer) const;
+        };
+}
+#endif // !_WIDGET_HPP
