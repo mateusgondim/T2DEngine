380f72fd7cf6c9920982e46e3c14fd22aa516ba6
mateusgondim <mateusgondimlima@gmail.com>
2019-12-24 14:20:12 -0300

    Remove unnecessary comments

--- engine/source/gfx/source/src/Animation_player.hpp
+++ engine/source/gfx/source/src/Animation_player.hpp
@@ -14,16 +14,12 @@ namespace gfx {
 	public:
 		Animation_player() : m_playing(false) {}
 		explicit Animation_player(const Animation & animation);
-		//void add_animation(const Animation & animation);
 		void update(const float dt);
 		void pause();
 		void resume();
 		void start_from_beg();
-		//void switch_animation(const std::vector<Animation>::size_type next_anim);
 		unsigned get_frame() const { return (m_current_frame < 0) ?(m_animation.get_frame(0)) :(m_animation.get_frame(m_current_frame) ); } //m_current_frame = -1 before first update
-		//unsigned get_anim() const { return m_current_animation; }
 		bool    is_playing() const { return m_playing; }
-		//std::pair<unsigned, unsigned> get_anim_n_frame() const { return std::pair<unsigned, unsigned>(m_current_animation, get_frame()); }
 		bool   changed_frame() const { return m_changed_frame; }
 	private:
 		Animation						   m_animation;
