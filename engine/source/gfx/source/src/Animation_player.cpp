9e8341ef6edf629da0888a50f7783a0f8989b25a
mateusgondim <mateusgondimlima@gmail.com>
2019-12-24 14:22:13 -0300

    Remove unnecessary comments

--- engine/source/gfx/source/src/Animation_player.cpp
+++ engine/source/gfx/source/src/Animation_player.cpp
@@ -13,17 +13,6 @@ gfx::Animation_player::Animation_player(const gfx::Animation & animation)
 	m_next_frame_time     = m_local_clock;
 }
 
-/*
-void gfx::Animation_player::add_animation(const Animation & animation)
-{
-	m_animations.push_back(animation);
-
-	if (m_animations.size() == 1) { //vector was empty
-		m_current_animation = 0;
-		m_current_frame = -1;
-		m_seconds_per_frame = 1.0f / m_animations[m_current_animation].frames_per_second();
-	}
-}*/
 
 void gfx::Animation_player::update(const float dt)
 {	
@@ -39,10 +28,6 @@ void gfx::Animation_player::update(const float dt)
 		m_changed_frame = false;
 		return;
 	}
-	/*
-	if ( (m_next_frame_time > g_timer.get_time()) || (!m_playing) ) { //if it is not yet time to change to the next frame 
-		return;
-	}*/
 	
 	++m_current_frame;
 	
@@ -56,10 +41,8 @@ void gfx::Animation_player::update(const float dt)
 			return;
 		}
 	}
-	//m_next_frame_time = g_timer.get_time() + m_seconds_per_frame; //calculate when to switch to next frame
 	m_next_frame_time   = m_local_clock + m_seconds_per_frame;
 	m_changed_frame = true;
-	//std::cout << "current frame: " << m_current_frame << "  next frame time: " << m_next_frame_time << std::endl;
 }
 
 //pauses the current animation
@@ -72,9 +55,6 @@ void gfx::Animation_player::resume()
 {
 	if (!m_playing) {
 		m_playing = true;
-		//m_local_clock = m_next_frame_time = 0.0f;
-		//m_next_frame_time = g_timer.get_time();// +m_seconds_per_frame;
-		//update();
 	}
 }
 
@@ -86,19 +66,6 @@ void gfx::Animation_player::start_from_beg()
 		m_current_frame = 0;
 		m_local_clock = 0.0f;
 		m_next_frame_time = m_seconds_per_frame;
-		//m_next_frame_time = g_timer.get_time() + m_seconds_per_frame;
-		//update();
 	}
 }
 
-/*
-void gfx::Animation_player::switch_animation(const std::vector<gfx::Animation>::size_type next_anim)
-{
-	m_current_animation = next_anim;
-
-	m_current_frame = -1;
-	m_seconds_per_frame = 1.0f / m_animations[m_current_animation].frames_per_second();
-	m_local_clock = m_next_frame_time = 0.0f;
-	m_changed_frame = true;
-	//m_next_frame_time = g_timer.get_time();
-}*/
