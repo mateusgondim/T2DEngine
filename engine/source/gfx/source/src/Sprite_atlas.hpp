72936ee3648f9f52a0886926636eb8caa70e3278
mateusgondim <mateusgondimlima@gmail.com>
2019-12-24 13:19:36 -0300

    Define Struct to store data of an atlas' image
    This commit, defines the Atlas_image struct, to store data regaring a single specific image, inside an atlas.

--- engine/source/gfx/source/src/Sprite_atlas.hpp
+++ engine/source/gfx/source/src/Sprite_atlas.hpp
@@ -16,6 +16,14 @@ namespace rms { class Resource_manager; }
 namespace gfx { class Texture_2d; class Texture_2d_manager; }
 
 namespace gfx {
+
+        // Struct containig data regarding a single image on the atlas
+        struct Atlas_image {
+                math::Rect      m_texture_coordinates;
+                int             m_width; // in pixels
+                int             m_height; // in pixels
+        };
+
 	class Sprite_atlas : public rms::Resource {
 	friend rms::Resource *gfx::Sprite_atlas_manager::load(const char *name, const char *file_path,
                                                           gfx::Texture_2d_manager *texture_manager);
