d27a5aa8b8ea1b83cac285ab91b46a76a9de5993
mateusgondim <mateusgondimlima@gmail.com>
2020-05-07 11:56:20 -0300

    Create vertex_data typedef
    This commit creates the vertex_data type, to be used by the get_view_space_vertices function, to return the necessary data to render a widget.

--- engine/source/ui/source/src/Widget.hpp
+++ engine/source/ui/source/src/Widget.hpp
@@ -6,6 +6,7 @@
 #include "Rect.hpp"
 #include "Vertex1P1C1UV.hpp"
 #include <cstddef>
+#include <utility>
 
 class Event;
 namespace ui { class Canvas; }
@@ -15,9 +16,11 @@ namespace ui
         class Widget : public gom::Game_object {
                 friend class Canvas;
         public:
-                virtual void update(const float dt) override;
-                virtual void on_event(Event & event) override;
-        private:
+                typedef std::pair<gfx::Vertex1P1C1UV *, std::size_t> vertex_data;
+
+                virtual void            update(const float dt) override;
+                virtual void            on_event(Event & event) override;
+        protected:
                 explicit Widget(Canvas & parent_canvas);
                          Widget(Canvas & parent_canvas, const math::Rect & rect,
                                 const std::size_t obj_sz = sizeof(Widget));
