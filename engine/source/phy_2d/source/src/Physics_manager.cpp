41ad2ff99862f06b5d2666c31b908c5489282046
mateusgondim <mateusgondimlima@gmail.com>
2020-06-02 20:45:01 -0300

    Refactor init function to remove Tile_map parameter
    This commit, removes the Tile_map parameter from the initialization function. This was necessary because the map is not yet available at the time of initialization.

--- engine/source/phy_2d/source/src/Physics_manager.cpp
+++ engine/source/phy_2d/source/src/Physics_manager.cpp
@@ -10,10 +10,9 @@ namespace physics_2d {
 
 	World * Physics_manager::get_world() { return m_pworld; }
 	
-	void Physics_manager::init(Tile_map *pmap, const math::vec2 & gravity)
+	void Physics_manager::init(const math::vec2 & gravity)
 	{
 		m_pworld = new World(gravity);
-		m_pworld->set_tile_map(pmap);
 	}
 	
 	void Physics_manager::shut_down() { delete m_pworld; }
