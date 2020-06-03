1af986fec3dd933db19012ebd271327204dabda4
mateusgondim <mateusgondimlima@gmail.com>
2020-06-03 17:35:29 -0300

    Add creator for the Demo's main menu

---
+++ game/source/src/Main_menu_creator.hpp
@@ -0,0 +1,20 @@
+#ifndef _MAIN_MENU_CREATOR_HPP
+#define _MAIN_MENU_CREATOR_HPP
+
+
+#include "Creator.hpp"
+#include "string_id.hpp"
+
+namespace gom  { class Game_object; }
+namespace math { struct vec3; }
+
+class Main_menu_creator final : public gom::Creator {
+public:
+        Main_menu_creator(const string_id canvas_atlas_id);
+        gom::Game_object * create(const math::vec3 & wld_pos) override;
+
+private:
+        string_id m_canvas_atlas_id;
+};
+
+#endif
