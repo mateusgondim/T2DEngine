47a4ca1172edba28f932f1e0a314c3924ed005ef
mateusgondim <mateusgondimlima@gmail.com>
2020-06-03 17:34:32 -0300

    Refactor to use translation vector for positioning

--- game/source/src/Level_ui_creator.cpp
+++ game/source/src/Level_ui_creator.cpp
@@ -25,17 +25,12 @@ gom::Game_object * Level_ui_creator::create(const math::vec3 & wld_pos)
         math::Rect screen_rect = gom::g_game_object_mgr.get_main_camera()->get_screen_rect();
         rms::Resource * pres = gfx::g_sprite_atlas_mgr.get_by_id(m_canvas_atlas_id);
         gfx::Sprite_atlas * pui_atlas = static_cast<gfx::Sprite_atlas*>(pres);
-        static_cast<gfx::Sprite_atlas*>(pres);
+
         ui::Canvas *pcanvas = ui::g_ui_mgr.create_canvas(screen_rect, *pui_atlas);
 
-        float width = screen_rect.width / 4.0f;
-        float height = screen_rect.height / 6.0f;
-        math::Rect widget_rect(screen_rect.center().x - width / 2.0f,
-                               screen_rect.center().y + height,
-                               width, height);
-
         void * pmem = mem::allocate(sizeof(ui::Text));
-        Pause_text *ptext = new (pmem) Pause_text(*pcanvas, widget_rect);
+        Pause_text *ptext = new (pmem) Pause_text(*pcanvas);
+        ptext->set_obj_to_canvas_translation(screen_rect.center());
         ptext->set_active(false);
 
         return pcanvas;
