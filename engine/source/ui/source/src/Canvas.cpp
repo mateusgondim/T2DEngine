65974965b016c0435b6df2aa95eb17da00ae7269
mateusgondim <mateusgondimlima@gmail.com>
2020-06-11 18:40:34 -0300

    Change line indentation

--- engine/source/ui/source/src/Canvas.cpp
+++ engine/source/ui/source/src/Canvas.cpp
@@ -37,67 +37,66 @@ namespace ui
         }
     }
 
-        void Canvas::update(const float dt) {}
-        void Canvas::on_event(Event & event) {}
+    void Canvas::update(const float dt) {}
+    void Canvas::on_event(Event & event) {}
 
-        Canvas * Canvas::add_widget(const Widget & widget)
-        {
-                if (m_num_widgets >= s_max_num_widgets) {
-                        return nullptr;
-                }
-
-                m_widgets[m_num_widgets++].bind(widget);
-                return this;
+    bool Canvas::add_widget(const Widget & widget)
+    {
+        if (m_num_widgets >= s_MAX_NUM_WIDGETS) {
+            return false;
         }
 
-        gfx::Sprite_atlas * Canvas::get_atlas()
-        {
-                rms::Resource * pres = gfx::g_sprite_atlas_mgr.get_by_id(m_atlas_id);
-                if (pres) {
-                        return static_cast<gfx::Sprite_atlas*>(pres);
-                }
-                return nullptr;
+        m_widgets[m_num_widgets++].bind(widget);
+        return true;
+    }   
+
+    gfx::Sprite_atlas * Canvas::get_atlas()
+    {
+        rms::Resource * pres = gfx::g_sprite_atlas_mgr.get_by_id(m_atlas_id);
+        if (pres) {
+            return static_cast<gfx::Sprite_atlas*>(pres);
         }
+            return nullptr;
+    }
 
-        void Canvas::render()
-        {
-                if (m_dirty) {
-                        // Clear the vertex buffer
-                        m_vertex_batch.reset();
+    void Canvas::render()
+    {
+        if (m_dirty) {
+            // Clear the vertex buffer
+            m_vertex_batch.reset();
 
-                        // recalculate the widgets vertices
-                        std::uint8_t i = 0;
-                        Widget::vertex_data rendering_data;
-                        gom::Game_object *pgame_object = nullptr;
-                        Widget *pwidget = nullptr;
-                        for (i; i != m_num_widgets; ++i) {
-                                pgame_object = gom::g_game_object_mgr.get_by_handle(m_widgets[i]);
-                                // We need to find a way to remove this handle if the object was destroyed!
-                                // Perhaps the best solution is indeed to use a std::vector
-                                if (!pgame_object) {
-                                        continue;
-                                }
-                                if (!pgame_object->is_active()) {
-                                        continue;
-                                }
+            // recalculate the widgets vertices
+            std::uint8_t i = 0;
+            Widget::vertex_data rendering_data;
+            gom::Game_object *pgame_object = nullptr;
+            Widget *pwidget = nullptr;
+            for (i; i != m_num_widgets; ++i) {
+                pgame_object = gom::g_game_object_mgr.get_by_handle(m_widgets[i]);
 
-                                pwidget = static_cast<Widget*>(pgame_object);
-                                rendering_data = pwidget->get_view_space_vertices();
-                                if (rendering_data.first == nullptr || rendering_data.second == 0) {
-                                        continue;
-                                }
-
-                                m_vertex_batch.add(rendering_data.first, rendering_data.second);
-                        }
-                        m_dirty = false;
+                if (!pgame_object) {
+                    continue;
                 }
-                m_vertex_batch.render();
-        }
-
-        Canvas::~Canvas()
-        {
-                for (std::uint8_t i = 0; i != m_num_widgets; ++i) {
-                        gom::g_game_object_mgr.request_destruction(m_widgets[i]);
+                if (!pgame_object->is_active()) {
+                    continue;
                 }
+
+                pwidget = static_cast<Widget*>(pgame_object);
+                rendering_data = pwidget->get_view_space_vertices();
+                if (rendering_data.first == nullptr || rendering_data.second == 0) {
+                    continue;
+                }
+
+                m_vertex_batch.add(rendering_data.first, rendering_data.second);
+            }
+            m_dirty = false;
         }
+        m_vertex_batch.render();
+    }
+
+    Canvas::~Canvas()
+    {
+        for (std::uint8_t i = 0; i != m_num_widgets; ++i) {
+            gom::g_game_object_mgr.request_destruction(m_widgets[i]);
+        }
+    }
 }
