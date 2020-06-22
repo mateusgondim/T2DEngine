a58dab19523a348faf6c3b6127aa7f2dbf75287c
mateusgondim <mateusgondimlima@gmail.com>
2020-06-22 11:05:58 -0300

    Implement in_collision method
    Add a implementation for the in_collision method on the Engine_collision_listener. This implementation works the same way as the begin and end collision callbacks. It will send a  'IN_COLLSION' event to each of the game objects.

--- engine/source/src/Engine_collision_listener.cpp
+++ engine/source/src/Engine_collision_listener.cpp
@@ -8,6 +8,7 @@
 Engine_collision_listener::Engine_collision_listener()
 {
        m_begin_collision_id = get_crc32("EVENT_BEGIN_COLLISION");
+       m_in_collision_id = get_crc32("EVENT_IN_COLLISION");
        m_end_collision_id = get_crc32("EVENT_END_COLLISION");
        m_game_object_id = get_crc32("game_object_id");
        m_game_object_handle_index = get_crc32("game_object_handle_index");
@@ -33,6 +34,16 @@ void Engine_collision_listener::begin_body_collision(physics_2d::Body_2d * pbody
 
         send_collision_event(pgame_object_a, pgame_object_b, m_begin_collision_id);
         send_collision_event(pgame_object_b, pgame_object_a, m_begin_collision_id);
+}
+
+void Engine_collision_listener::in_body_collision(physics_2d::Body_2d * pbody_a,
+                                                  physics_2d::Body_2d * pbody_b) const
+{
+        gom::Game_object  *pgame_object_a = static_cast<gom::Game_object*>(pbody_a->get_user_data());
+        gom::Game_object  *pgame_object_b = static_cast<gom::Game_object*>(pbody_b->get_user_data());
+
+        send_collision_event(pgame_object_a, pgame_object_b, m_in_collision_id);
+        send_collision_event(pgame_object_b, pgame_object_a, m_in_collision_id);
 }
 
 void Engine_collision_listener::end_body_collision(physics_2d::Body_2d * pbody_a,
--- engine/source/src/Engine_collision_listener.hpp
+++ engine/source/src/Engine_collision_listener.hpp
@@ -6,17 +6,24 @@
 namespace physics_2d { class Body_2d; }
 namespace gom { class Game_object; }
 
-class Engine_collision_listener : public physics_2d::Collision_listener {
+class Engine_collision_listener final : public physics_2d::Collision_listener {
 public:
         Engine_collision_listener();
-        void begin_body_collision(physics_2d::Body_2d * pbody_a, physics_2d::Body_2d * pbody_b) const override;
-        void end_body_collision(physics_2d::Body_2d * pbody_a, physics_2d::Body_2d * pbody_b) const override;
+        void begin_body_collision(physics_2d::Body_2d * pbody_a,
+                                  physics_2d::Body_2d * pbody_b) const override;
+
+        void in_body_collision(physics_2d::Body_2d * pbody_a,
+                               physics_2d::Body_2d * pbody_b) const override;
+
+        void end_body_collision(physics_2d::Body_2d * pbody_a,
+                                physics_2d::Body_2d * pbody_b) const override;
 private:
         void  send_collision_event(gom::Game_object * pfrom_object, 
                                    gom::Game_object * pto_object,
                                    std::uint32_t event_type) const;
 
         std::uint32_t           m_begin_collision_id;
+        std::uint32_t           m_in_collision_id;
         std::uint32_t           m_end_collision_id;
         std::uint32_t           m_game_object_id;
         std::uint32_t           m_game_object_handle_index;
